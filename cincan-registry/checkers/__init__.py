b8f5f4631c21684cd21760f0dcd13d446eb6e1b6
Karoliina Kemppainen <kemppainenkaro@gmail.com>
2020-04-07 08:28:41 +0300

    Added debian checker

--- cincan-registry/checkers/__init__.py
+++ cincan-registry/checkers/__init__.py
@@ -7,10 +7,11 @@
 from .github import GitHubChecker
 from .bitbucket import BitbucketChecker
 from .pypi import PypiChecker
+from .debian import DebianChecker
 
-classmap = {"github": GitHubChecker, "bitbucket": BitbucketChecker, "pypi": PypiChecker}
+classmap = {"github": GitHubChecker, "bitbucket": BitbucketChecker, "pypi": PypiChecker, "debian": DebianChecker}
 
-__all__ = ["GitHubChecker", "BitbucketChecker", "PypiChecker"]
+__all__ = ["GitHubChecker", "BitbucketChecker", "PypiChecker", "DebianChecker"]
 
 # for (_, name, _) in pkgutil.iter_modules([Path(__file__).parent]):
 #     imported_module = import_module("." + name, package=__name__)

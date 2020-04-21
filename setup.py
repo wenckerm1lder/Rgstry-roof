3ce0d56b8f0bf99e7c17cf6e76d23ccd1a0c92eb
Niklas Saari <niklas.saari@tutanota.com>
2020-04-21 20:36:57 +0300

    setup.py fixed

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("VERSION", "r") as ver:
     version_info = ver.read().strip()
 
 setup(
-    name='cincan-registry',
+    name='cinverman',
     version=version_info,
     author="",
     author_email="",
-    description='Tool for listing available CinCan tools, their versions and possible updates.',
+    description='CinCan Version Manager: a tool for listing available CinCan tools, their versions and possible updates.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/cincan/cincan-registry",
-    packages=['cincan-registry'],
+    packages=['cinverman'],
     install_requires=['docker>=4.1'],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,7 +23,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     entry_points={
-        'console_scripts': ['cincan-registry=cincan-registry.__main__:main'],
+        'console_scripts': ['cinverman=cinverman.__main__:main'],
     },
     python_requires='>=3.6',
 )

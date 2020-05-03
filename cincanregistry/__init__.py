bef7af52fedc00fa78fb757fdbf0b7e404cee04c
Niklas Saari <niklas.saari@tutanota.com>
2020-05-03 23:46:25 +0300

    Change to work as module as well

--- cincanregistry/__init__.py
+++ cincanregistry/__init__.py
@@ -2,6 +2,6 @@ from .version_info import VersionInfo
 from .tool_info import ToolInfo, ToolInfoEncoder
 from .version_maintainer import VersionMaintainer
 from .registry import ToolRegistry
-
+from .main import list_handler, create_argparse
 
 __all__ = ["ToolInfo", "VersionInfo", "ToolRegistry", "VersionMaintainer"]

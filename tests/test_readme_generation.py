13a5b344e099138025c2ca39454d7c427183b1d6
Niklas Saari <niklas.saari@tutanota.com>
2020-05-23 22:24:00 +0300

    Fix initial readme test

--- tests/test_readme_generation.py
+++ tests/test_readme_generation.py
@@ -5,5 +5,7 @@ def test_update_tool_readme(mocker, tmpdir):
     p = tmpdir.mkdir("my_tool").join("README.md")
     p.write("# This is useful example tool.")
     reg = HubReadmeHandler()
-    mocker.patch.object(reg, "_is_docker_running", return_value=True, autospec=True)
+    mocker.patch.object(
+        reg.client, "ping", return_value=True, autospec=True,
+    )
     print(reg._is_docker_running())

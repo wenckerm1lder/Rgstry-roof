6d76492fab9e26a46c0129294f4f4b19d735ebc5
Niklas Saari <niklas.saari@tutanota.com>
2020-05-25 01:01:42 +0300

    Minor tests for readme instance

--- tests/test_readme_generation.py
+++ tests/test_readme_generation.py
@@ -1,4 +1,12 @@
+import pytest
+import pathlib
 from cincanregistry import HubReadmeHandler
+
+
+def test_create_hub_readme_handler():
+    reg = HubReadmeHandler()
+    assert reg.max_size == 25000
+    assert reg.max_description_size == 100
 
 
 def test_update_tool_readme(mocker, tmpdir):
@@ -8,4 +16,10 @@ def test_update_tool_readme(mocker, tmpdir):
     mocker.patch.object(
         reg.client, "ping", return_value=True, autospec=True,
     )
-    # print(reg._is_docker_running())
+    with pytest.raises(RuntimeError):
+        reg.update_readme_all_tools()
+
+    mocker.patch.object(reg, "_get_hub_session_cookies", return_value=True)
+    reg.tools_repo_path = pathlib.Path("some/invalid/path")
+    # TODO sometime
+

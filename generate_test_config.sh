21d615299246aa84dd9e99eb7105eeb5cb36b498
Niklas Saari <niklas.saari@tutanota.com>
2021-04-20 15:45:55 +0300

    Add temporal url

--- generate_test_config.sh
+++ generate_test_config.sh
@@ -26,7 +26,8 @@ EOF
 echo "Configuration file created into path: ${CI_PROJECT_DIR}/.cincan/cache"
 mkdir -p "$CACHE_DIR"
 
-ARTIFACT_LOCATION="https://gitlab.com/CinCan/cincan-registry/-/jobs/artifacts/master/raw/.cincan/cache/tooldb.sqlite?job=scrape_versions:on-schedule"
+# ARTIFACT_LOCATION="https://gitlab.com/CinCan/cincan-registry/-/jobs/artifacts/master/raw/.cincan/cache/tooldb.sqlite?job=scrape_versions:on-schedule"
+ARTIFACT_LOCATION="https://gitlab.com/CinCan/cincan-registry/-/jobs/1189807637/artifacts/raw/.cincan/cache/tooldb.sqlite?inline=false"
 status_code=$(curl -L --write-out %{http_code} --silent --output /dev/null "$ARTIFACT_LOCATION")
 
 if [[ "$status_code" -eq 200 ]] ; then
